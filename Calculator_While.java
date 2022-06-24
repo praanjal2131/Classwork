b3059fde785f6e30974e7ce05566db255ddb8e1c
Pranjali Khairnar <106724039+praanjal2131@users.noreply.github.com>
2022-06-24 18:47:16 +0530

    Add files via upload

---
+++ Calculator_While.java
@@ -0,0 +1,70 @@
+import java.util.*;
+class Calculator_While
+{
+	public static void main(String[] args)
+	{   
+                    int i=1;
+		Scanner sc=new Scanner(System.in);
+
+		System.out.print("\n\t\t\t    Calculator  ");
+		System.out.print("\n\t\t\t================\n\n");
+
+		System.out.print("\n\t\t\t+:ADDITION ");
+		System.out.print("\n\t\t\t-:SUBSTRACTION ");
+		System.out.print("\n\t\t\t*:MULTIPLICATION ");
+		System.out.print("\n\t\t\t/:DIVISION ");
+		System.out.print("\n\t\t\t================");
+                while(i!=0)
+              {
+		System.out.print("\n\t\t\tEnter the choice: ");
+		char c=sc.next().charAt(0);
+
+		System.out.print("\n\t\t\tEnter number1=");
+		int num1=sc.nextInt();
+
+		System.out.print("\n\t\t\tEnter number2=");
+		int num2=sc.nextInt();
+
+
+		switch(c)
+		{
+			case '+':
+			{
+				int result =num1+num2;
+				System.out.print("\n\t\t\tAddition Result= "+result);
+			break;
+			}
+
+			case '-' :
+			{
+				int result =num1-num2;
+				System.out.print("\n\t\t\tSubstraction Result= "+result);
+			break;
+			}
+
+			case '*' :
+			{
+				int result =num1*num2;
+				System.out.print("\n\t\t\tMultiplication Result= "+result);
+			break;
+			}
+
+			case '/' :
+			{
+				double result =num1/num2;
+				System.out.print("\n\t\t\tDivision Result= "+result);
+			break;
+			}
+
+			default :
+			{
+				System.out.print("\n\t\t\tEntered wrong choice.....");
+			break;
+			}
+		}//switch
+                  System.out.println("\n  Do  you want to continue press 1  or 0( Terminate)");
+                 i=sc.nextInt();
+           }//while
+
+	}
+}
